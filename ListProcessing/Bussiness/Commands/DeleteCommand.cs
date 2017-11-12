1036a46201888c09acf83337d04cf481b5ecd6a4
iarnaudov91 <iarnaudov91@gmail.com>
2017-11-12 21:15:54 +0200

    Delete and Reverse Command Added

---
+++ ListProcessing/Bussiness/Commands/DeleteCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListProcessing.Bussiness.Commands
+{
+    class DeleteCommand : Command
+    {
+
+        public DeleteCommand(string[] data, List<string> items)
+            : base(data, items)
+        {
+        }
+
+        public override string Execute()
+        {
+            int index = int.Parse(this.Data[0]);
+
+            if (index >= 0 && index <= this.Items.Count-1)
+            {
+                this.Items.RemoveAt(index);
+            }
+            else
+            {
+                Console.WriteLine($"Error: invalid index {index}");
+            }
+
+            return string.Join(" ", this.Items);
+        }
+    }
+}
---
+++ ListProcessing/Bussiness/Commands/ReverseCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListProcessing.Bussiness.Commands
+{
+    class ReverseCommand : Command
+    {
+
+        public ReverseCommand(List<string> items)
+            : base(items)
+        {
+        }
+
+        public override string Execute()
+        {
+            this.Items.Reverse();
+
+            return string.Join(" ", this.Items);
+        }
+    }
+    
+}
